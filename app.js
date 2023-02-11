04783ce16bf38186449166f4bce44bffe565e03d
Pierre Da Costa <pierre@Mac-mini-de-Pierre.local>
2023-02-11 15:22:24 -0300

    add the project base

---
+++ app.js
@@ -0,0 +1,20 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('express-async-errors')
+
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+
+if(process.env.NODE_ENV === 'development')
+  app.use(middleware.requestLogger)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
