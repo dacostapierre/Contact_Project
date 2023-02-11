ad139cb819c6d449155460d30e0c0ac07bcdd8a7
Pierre Da Costa <pierre@Mac-mini-de-Pierre.local>
2023-02-11 16:00:49 -0300

    uibuild

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const logger = require('./utils/logger')
 const server = http.createServer(app)
 
 server.listen(config.PORT, () => {
+  logger.info('Hello')
   logger.info(`Server running on port ${config.PORT}`)
 })
