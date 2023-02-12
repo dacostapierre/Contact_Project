8f15ccb59337419be6a4cee1e8a8bba6a477a007
Pierre Da Costa <pierre@Mac-mini-de-Pierre.local>
2023-02-12 20:09:15 -0300

    add base of web site

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const app = express()
 const cors = require('cors')
 require('express-async-errors')
 
-const contactRouter = require('./controllers/contact')
 const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
 
 app.use(cors())
 app.use(express.static('build'))
@@ -15,7 +13,7 @@ app.use(express.json())
 if(process.env.NODE_ENV === 'development')
   app.use(middleware.requestLogger)
 
-app.use('/api', contactRouter)
+
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- controllers/contact.js
+++
@@ -1,8 +0,0 @@
-const userRouter = require('express').Router()
-const bcrypt = require('bcrypt')
-
-userRouter.get('/', async (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
-module.exports = userRouter
